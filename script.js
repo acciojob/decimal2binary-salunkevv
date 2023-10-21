7909dccc9d003f09eedf542706a8491c701a82fb
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-10-21 16:58:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function decimalToBinary(num) {
   //Write you code here
+	  var quotient =Math.floor(num/2);
+      let binary="";
+   
+      while(quotient>=2)
+          {
+            let remainder = num %2 ;
+            binary = binary + remainder; 
+            quotient = Math.floor(num/2);
+            num= quotient;
+          }
+      binary+=1;
+      let ans =[...binary].reverse().join("");
+      console.log(ans);
   
 }
 
